8f13b99b1b026bce4f25e80dce627ac8fbdd9e4d
Nicholas Cotter <nick@extropy.net>
2018-05-09 14:18:55 +0100

    wip on search

--- GutenbergSentiment/server.R
+++ GutenbergSentiment/server.R
@@ -1,26 +1,37 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(gutenbergr)
+library(dplyr)
+library(stringr)
+library(tidytext)
+library(tidyr)
+library(wordcloud)
+library(shinyWidgets)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+  
+  books <- reactive({
+    gutenberg_works(str_detect(author, input$searchTerms)) 
   })
   
+#  observeEvent(input$searchButton, {
+#    books <- gutenberg_works(str_detect(author, input$searchTerms))   
+#  })
+  
+  output$bookSelector <- renderUI({
+    selectInput("bookIds", "Books", books()$title, multiple=TRUE)
+  })
+    
+  #output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+   # x    <- faithful[, 2] 
+  #  bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+  #  hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  #})
+  
 })
+
